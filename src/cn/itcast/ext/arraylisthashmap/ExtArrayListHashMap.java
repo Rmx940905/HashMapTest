8c7dd0a7a681bb4d326d31a337088cb74e57e383
RMX <1454552467@qq.com>
2018-11-23 19:21:22 +0800

    从新整理

--- src/cn/itcast/ext/arraylisthashmap/ExtArrayListHashMap.java
+++ src/cn/itcast/ext/arraylisthashmap/ExtArrayListHashMap.java
@@ -1,22 +1,25 @@
 package cn.itcast.ext.arraylisthashmap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //基于ArrayList实现HashMap(效率低)
 public class ExtArrayListHashMap<Key,Value> {
 
     //Map存储容量
-    private List<cn.itcast.linklisthashmap.Entry<Key,Value>> list = new ArrayList<cn.itcast.linklisthashmap.Entry<Key,Value>>();
+    private List<Entry<Key,Value>> list = new ArrayList<Entry<Key,Value>>();
 
     //HashMap的put方法
     public void put(Key key,Value value){
         //获取Entry对象
-        cn.itcast.linklisthashmap.Entry entry = getEntry(key);
+        Entry entry = getEntry(key);
         //判断是否为空
         if(entry != null){
             //不为空将Value值替换
             entry.value = value;
         }else {
             //为空创建Entry对象储存到list中
-            cn.itcast.linklisthashmap.Entry newEntry = new cn.itcast.linklisthashmap.Entry(key,value);
+            Entry newEntry = new Entry(key,value);
             //调用put的时候，将该hash储存对象存到ArrayList中
             list.add(newEntry);
         }
@@ -25,12 +28,12 @@ public class ExtArrayListHashMap<Key,Value> {
 
     //HashMap的get方法
     public Value get(Key key){
-        cn.itcast.linklisthashmap.Entry<Key,Value> entry = getEntry(key);
+        Entry<Key,Value> entry = getEntry(key);
         return entry == null ? null : entry.value;
     }
 
-    public cn.itcast.linklisthashmap.Entry<Key,Value> getEntry(Key key){
-        for (cn.itcast.linklisthashmap.Entry<Key,Value> entry : list){
+    public Entry<Key,Value> getEntry(Key key){
+        for (Entry<Key,Value> entry : list){
             if (entry.key.equals(key)){
                 return entry;
             }
--- src/cn/itcast/linklisthashmap/ExtLinkListHashMap.java
+++ src/cn/itcast/linklisthashmap/ExtLinkListHashMap.java
@@ -5,6 +5,12 @@ public class ExtLinkListHashMap {
 
     Entry[] tables = new Entry[998];
 
+    public void put(Object key,Object value){
+        int hashCode = key.hashCode();
+        //两个对象最比较的时候，如果hashCode相同，对象的的值不一定相同。
+        //两个对象最比较的时候，如果equest比较相同，对象的的值相同。
+
+    }
 
 }
 
